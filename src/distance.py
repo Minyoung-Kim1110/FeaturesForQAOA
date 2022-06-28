a621c0e71b250b22230b8bdaf533a86fee549c1a
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-06-28 19:45:25 +0900

    quantum feature map added

--- src/distance.py
+++ src/distance.py
@@ -20,3 +20,13 @@ def L1(x, y):
     return np.sum(np.abs(x-y))
 
 distance_func_list = [ cos_dist, L2, L1 ]
+
+class QuantumKernelMap():
+    def __init__(self, backend):
+        self.backend = backend
+
+    def run(data:np.array):
+        num_datapt, num_features = data.shape
+        feature_map =ZZFeatureMap(feature_dimension=num_features, reps=2, entanglement="linear")
+        kernel = QuantumKernel(feature_map=feature_map, quantum_instance=self.backend)
+        return kernel.evaluate(data)
